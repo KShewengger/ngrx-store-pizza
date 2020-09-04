fede92d227c0400c40cdf18ce153f16bd7057f27
Kristy Mae Almuete <kristyalmuete@gmail.com>
2020-09-04 16:30:46 +0800

    Setup Toppings Effect Functionality

---
+++ src/app/products/store/effects/toppings.effect.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+import { Effect, Actions, ofType } from '@ngrx/effects';
+
+import { of } from 'rxjs';
+import { map, catchError, switchMap } from 'rxjs/operators';
+
+import { LOAD_TOPPINGS, LoadToppingsSuccess, LoadToppingsFail } from '@products/store/actions/toppings.action';
+import { ToppingsService } from '@products/services/toppings.service';
+
+
+@Injectable()
+export class ToppingsEffects {
+
+  constructor(
+    private actions$: Actions,
+    private toppingsService: ToppingsService
+  ) {}
+
+  @Effect()
+  loadToppings$ = this.actions$
+    .pipe(
+      ofType(LOAD_TOPPINGS),
+      switchMap(() => this.toppingsService
+        .getToppings()
+        .pipe(
+          map(toppings => new LoadToppingsSuccess(toppings)),
+          catchError(error => of(new LoadToppingsFail(error)))
+        )
+      )
+    );
+
+}
+
